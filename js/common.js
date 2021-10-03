025fd69021e58003f70f8c017f48b4428bb848fb
zhzhdkchzh <81783023+zhzhdkchzh@users.noreply.github.com>
2021-10-03 20:24:43 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,3 +1,8 @@
-/**
- * 
- */
+$(document).on('click', '#ins', function(){
+	location.href = 'insurance/insurance.html'
+});
+$(document).on('click', '.sub-page', function(){
+	location.href = '../home.html'
+});
+
+
